3fc6fd9f68af78da7f2f4faed596868ab9b0a99c
AbielBorja <abielboga@gmail.com>
2025-02-27 15:55:25 -0600

    Change gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
 
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.livedata.ktx)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/example/rickandmorty/repository/RickAndMortyRepository.kt
+++ app/src/main/java/com/example/rickandmorty/repository/RickAndMortyRepository.kt
@@ -2,6 +2,8 @@ package com.example.rickandmorty.repository
 
 import com.example.rickandmorty.network.RickAndMortyCharactersApiService
 
-class RickAndMortyRepository(private val rickAndMortyCharactersApiService: RickAndMortyCharactersApiService) {
+class RickAndMortyRepository(
+    private val rickAndMortyCharactersApiService: RickAndMortyCharactersApiService
+) {
     suspend fun getCharacters(page: String) = rickAndMortyCharactersApiService.fetchCharacters(page)
 }
